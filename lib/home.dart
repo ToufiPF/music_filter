c21388fe6d7d34188657c3da97b45d6e27085007
Aurélien Clergeot <aurelien.clergeot@epfl.ch>
2023-06-10 13:08:00 +0200

    Add PermissionsNotifier

---
+++ lib/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: theme.colorScheme.inversePrimary,
+          title: Text(MyApp.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/providers/permissions.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/widgets.dart';
+import 'package:permission_handler/permission_handler.dart' as ph;
+
+/// Available permissions for the app
+enum PermissionGroup {
+  storage(ph.Permission.storage),
+  notif(ph.Permission.notification),
+  media(ph.Permission.mediaLibrary);
+
+  const PermissionGroup(this._perm);
+
+  final ph.Permission _perm;
+}
+
+enum PermissionStatus {
+  /// Permission has been permanently denied
+  permanentlyDenied,
+
+  /// Permission is not granted
+  denied,
+
+  /// Permission granted
+  granted;
+
+  static PermissionStatus _from(ph.PermissionStatus status) => status.isGranted
+      ? PermissionStatus.granted
+      : status.isPermanentlyDenied
+          ? PermissionStatus.permanentlyDenied
+          : PermissionStatus.denied;
+}
+
+class PermissionsNotifier extends ChangeNotifier {
+  static const String tag = "PermissionsNotifier";
+
+  /// Open the app settings
+  static Future<bool> openSettings() => ph.openAppSettings();
+
+  /// Holds status of each permission type
+  final _statuses = <PermissionGroup, PermissionStatus>{};
+
+  PermissionsNotifier() {
+    refreshStatus(PermissionGroup.values);
+  }
+
+  /// Returns the status for the given permission
+  PermissionStatus getStatus(PermissionGroup permission) =>
+      _statuses[permission] ?? PermissionStatus.denied;
+
+  /// Returns the statuses for the given permissions, in the same order
+  List<PermissionStatus> getStatuses(Iterable<PermissionGroup> permissions) =>
+      permissions.map((e) => getStatus(e)).toList(growable: false);
+
+  /// Requests for the given permissions,
+  /// automatically refreshes status afterwards.
+  Future<void> requestPermissions(List<PermissionGroup> permissions) async {
+    final perms = <PermissionStatus>[];
+    for (var t in permissions) {
+      perms.add(PermissionStatus._from(await t._perm.request()));
+    }
+    await _updateInternal(permissions, perms);
+  }
+
+  /// Requests for the given permissions, and go to settings if permission is permanently denied
+  /// Page calling this method needs to manually refresh the permissions when it resumes
+  Future<void> requestOrGoToSettings(List<PermissionGroup> permissions) async {
+    final perms = <PermissionStatus>[];
+    for (var t in permissions) {
+      perms.add(PermissionStatus._from(await t._perm.request()));
+    }
+    if (perms.any((e) => e == PermissionStatus.permanentlyDenied)) {
+      var ok = await openSettings();
+      if (!ok) {
+        debugPrint("[$tag]_requestOrGoToSettings: failed to launch settings");
+      }
+    }
+  }
+
+  /// Refresh the status of all permissions
+  Future<void> refreshStatus([List<PermissionGroup>? permissions]) async {
+    final perms = <PermissionStatus>[];
+    for (var t in PermissionGroup.values) {
+      perms.add(PermissionStatus._from(await t._perm.status));
+    }
+    await _updateInternal(PermissionGroup.values, perms);
+  }
+
+  Future<void> _updateInternal(
+    List<PermissionGroup> types,
+    List<PermissionStatus> permissions,
+  ) async {
+    debugPrint('[$tag]_updateInternal($types) --> $permissions');
+    assert(types.length == permissions.length);
+
+    bool shouldNotify = false;
+    for (var (idx, type) in types.indexed) {
+      PermissionStatus? oldStatus = _statuses[type];
+      PermissionStatus newStatus = permissions[idx];
+
+      if (oldStatus != newStatus &&
+          !(oldStatus == PermissionStatus.permanentlyDenied &&
+              newStatus == PermissionStatus.denied)) {
+        _statuses[type] = newStatus;
+        shouldNotify = true;
+      }
+    }
+
+    if (shouldNotify) {
+      notifyListeners();
+    }
+  }
+}
