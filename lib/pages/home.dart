d5dba349ea63f1b6ae423efc7a57b1dfdcdb3e5e
Aurélien Clergeot <aurelien.clergeot@epfl.ch>
2023-06-17 18:47:01 +0200

    Remove appBar title to get more space

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../main.dart';
 import '../providers/active_tabs.dart';
 import '../providers/root_folder.dart';
 import '../settings/active_tabs.dart';
 import '../settings/settings.dart';
 import '../widget/file_view.dart';
+import '../widget/player_widget.dart';
 import '../widget/queue_view.dart';
-import 'app_bar.dart';
 
 /// Home page for the app
 class HomePage extends StatefulWidget {
@@ -25,24 +24,24 @@ class _HomePageState extends State<HomePage> {
     return Consumer<ActiveTabsNotifier>(
         builder: (context, activeTabs, child) => DefaultTabController(
               length: activeTabs.activeTabs.length,
+              animationDuration: Duration.zero,
               child: Scaffold(
-                  appBar: AppBar(
-                    backgroundColor: theme.colorScheme.inversePrimary,
-                    title: Text(activeTabs.selected.appBarTitle ?? MyApp.title),
-                    actions: [
-                      AppBarUtils.settingsAction(context),
-                    ],
-                    bottom: TabBar(tabs: [
+                appBar: AppBar(
+                  backgroundColor: theme.colorScheme.inversePrimary,
+                  flexibleSpace: SafeArea(
+                    child: TabBar(tabs: [
                       for (var tab in activeTabs.activeTabs)
                         Tab(icon: Icon(tab.iconDefault), text: tab.label)
                     ]),
                   ),
-                  body: TabBarView(
-                    children: [
-                      for (var tab in activeTabs.activeTabs)
-                        _bodyForTab(context, tab)
-                    ],
-                  )),
+                ),
+                body: TabBarView(
+                  children: [
+                    for (var tab in activeTabs.activeTabs)
+                      _bodyForTab(context, tab)
+                  ],
+                ),
+              ),
             ));
   }
 
