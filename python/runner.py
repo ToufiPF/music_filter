977b1ec74d01f9a45ba48a03618e2e242189c836
Aurélien Clergeot <aurelien.clergeot@epfl.ch>
2023-06-30 18:46:56 +0200

    Reorganize flutter code in subdir

---
+++ python/runner.py
@@ -0,0 +1,12 @@
+import argparse as ap
+
+def setup_parser() -> 'ap.ArgumentParser':
+    return None
+
+if __name__ == '__main__':
+
+    parser = setup_parser()
+
+    args = parser.parse_args()
+
+    
