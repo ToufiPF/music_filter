73af95eb65254fbdd5dafa9ece75f42a965e9834
Aurélien Clergeot <aurelien.clergeot@epfl.ch>
2023-07-01 21:18:31 +0200

    QueueView: icon buttons to save/delete music

--- flutter/lib/widget/queue_view.dart
+++ flutter/lib/widget/queue_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../models/music.dart';
+import '../models/store.dart';
 import '../providers/player.dart';
 import '../providers/playlist.dart';
 import 'context_menu.dart';
@@ -14,54 +16,85 @@ class QueueView extends StatelessWidget {
   @override
   Widget build(BuildContext context) =>
       Consumer2<PlayerQueueNotifier, PlayerStateController>(
-          builder: (context, queue, player, child) => queue.queue.isEmpty
-              ? Text("Nothing to play !\n"
-                  "Start by adding some songs to the queue")
-              : ListView.builder(
-                  itemCount: queue.queue.length,
-                  itemBuilder: (context, musicIdx) {
-                    final music = queue.queue[musicIdx];
-                    // Trigger rebuild only if currently played index goes to != to == or vice-versa
-                    return StreamBuilder<bool>(
-                        initialData: player.indexInPlaylist == musicIdx,
-                        stream: player.indexInPlaylistStream
-                            .map((currentIdx) => currentIdx == musicIdx),
-                        builder: (context, snapshot) {
-                          final isSongPlaying = snapshot.requireData;
-                          return ListTile(
-                            selected: isSongPlaying,
-                            title: Text(music.title ?? music.filename),
-                            subtitle: Text(music.displayArtist),
-                            leading: Icon(isSongPlaying
-                                ? Icons.play_arrow
-                                : Icons.play_arrow_outlined),
-                            trailing: PopupMenuButton<int>(
-                              child: Icon(Icons.more_vert, size: 32),
-                              itemBuilder: (context) => [
-                                for (var action in popupActions)
-                                  PopupMenuItem<int>(
-                                      value: action.index,
-                                      child: Text(action.text)),
-                              ],
-                              onSelected: (selectedAction) =>
-                                  _onPopupMenuAction(
-                                context,
-                                queue,
-                                player,
-                                MenuAction.values[selectedAction],
-                                musicIdx,
-                              ),
-                            ),
-                            onTap: () {
-                              if (isSongPlaying && player.isPlaying) {
-                                return;
-                              }
+          builder: (context, queue, player, child) {
+        final store = Provider.of<Store>(context, listen: false);
+        return queue.queue.isEmpty
+            ? Center(
+                child: Text("Nothing to play !\n"
+                    "Start by adding some songs to the queue"))
+            : ListView.builder(
+                itemCount: queue.queue.length,
+                // Trigger rebuild only if currently played index goes to != to == or vice-versa
+                itemBuilder: (context, musicIdx) => StreamBuilder<bool>(
+                    initialData: player.indexInPlaylist == musicIdx,
+                    stream: player.indexInPlaylistStream
+                        .map((currentIdx) => currentIdx == musicIdx),
+                    builder: (context, snapshot) => _buildTile(
+                          context,
+                          queue,
+                          player,
+                          store,
+                          musicIdx,
+                          snapshot.requireData,
+                        )));
+      });
 
-                              player.play(index: musicIdx);
-                            },
-                          );
-                        });
-                  }));
+  Widget _buildTile(
+    BuildContext context,
+    PlayerQueueNotifier queue,
+    PlayerStateController player,
+    Store store,
+    int musicIdx,
+    bool isSongPlaying,
+  ) {
+    final music = queue.queue[musicIdx];
+    return ListTile(
+      selected: isSongPlaying,
+      title: Text(music.title ?? music.filename),
+      subtitle: Text(music.displayArtist),
+      leading:
+          Icon(isSongPlaying ? Icons.play_arrow : Icons.play_arrow_outlined),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: () => store.markAs(music, KeepState.kept),
+            icon: Icon(Icons.save),
+          ),
+          IconButton(
+            onPressed: () => store.markAs(music, KeepState.deleted),
+            icon: Icon(Icons.delete_forever),
+          ),
+          PopupMenuButton<int>(
+            child: Icon(Icons.more_vert, size: 32),
+            itemBuilder: (context) => [
+              for (var action in popupActions)
+                PopupMenuItem<int>(
+                    value: action.index, child: Text(action.text)),
+            ],
+            onSelected: (selectedAction) => _onPopupMenuAction(
+              context,
+              queue,
+              player,
+              MenuAction.values[selectedAction],
+              musicIdx,
+            ),
+          ),
+        ],
+      ),
+      onTap: () {
+        // if song is currently playing, don't call play()
+        // as this would reset the time to 0
+        if (isSongPlaying && player.isPlaying) {
+          return;
+        }
+
+        player.play(index: musicIdx);
+      },
+    );
+  }
 
   Future<void> _onPopupMenuAction(
     BuildContext context,
