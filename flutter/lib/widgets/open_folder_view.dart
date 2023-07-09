6c5dab3219627ab1db0a4adf78bd385880ffaa45
Aurélien Clergeot <aurelien.clergeot@epfl.ch>
2023-07-09 10:16:46 +0200

    Renamed bug_slice => slice_bug + OpenFolderView removed unused var

--- flutter/lib/widgets/open_folder_view.dart
+++ flutter/lib/widgets/open_folder_view.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../models/catalog.dart';
 import '../models/state_store.dart';
 import 'context_menu.dart';
 
@@ -24,55 +23,54 @@ class _OpenMusicViewState extends State<OpenMusicView> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    final catalog = Provider.of<Catalog>(context, listen: false);
-    return WillPopScope(
-      onWillPop: () async {
-        if (path.isNotEmpty) {
-          setState(() => path = Directory(path).parent.path);
-          return false;
-        }
-        return true;
-      },
-      child: Consumer<StateStore>(builder: (builder, store, child) {
-        final root = store.openFoldersHierarchy;
-        final toDisplay = root.lookup(path)!;
-        return ListView.builder(
-          itemCount: toDisplay.children.length + toDisplay.musics.length,
-          itemBuilder: (context, idx) {
-            if (idx < toDisplay.children.length) {
-              final folder = toDisplay.children[idx];
-              return ListTile(
-                title: Text(folder.folderName),
-                onTap: () => setState(() => path = folder.path),
-                trailing: IconActions.exportActionFolder(context, folder),
-              );
-            } else {
-              idx -= toDisplay.children.length;
-              final music = toDisplay.musics[idx];
-              return ListTile(
-                title: Text(music.title ?? music.filename),
-                trailing: StreamBuilder<KeepState>(
-                    initialData: KeepState.unspecified,
-                    stream: store.watchState(music, fireImmediately: true),
-                    builder: (context, snapshot) {
-                      final state = snapshot.data!;
-                      return Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          IconActions.keptMusicAction(context, music, state),
-                          IconActions.deleteMusicAction(context, music, state),
-                          IconActions.exportActionMusic(context, music, state),
-                        ],
-                      );
-                    }),
-              );
-            }
-          },
-        );
-      }),
-    );
-  }
+  Widget build(BuildContext context) => WillPopScope(
+        onWillPop: () async {
+          if (path.isNotEmpty) {
+            setState(() => path = Directory(path).parent.path);
+            return false;
+          }
+          return true;
+        },
+        child: Consumer<StateStore>(builder: (builder, store, child) {
+          final root = store.openFoldersHierarchy;
+          final toDisplay = root.lookup(path)!;
+          return ListView.builder(
+            itemCount: toDisplay.children.length + toDisplay.musics.length,
+            itemBuilder: (context, idx) {
+              if (idx < toDisplay.children.length) {
+                final folder = toDisplay.children[idx];
+                return ListTile(
+                  title: Text(folder.folderName),
+                  onTap: () => setState(() => path = folder.path),
+                  trailing: IconActions.exportActionFolder(context, folder),
+                );
+              } else {
+                idx -= toDisplay.children.length;
+                final music = toDisplay.musics[idx];
+                return ListTile(
+                  title: Text(music.title ?? music.filename),
+                  trailing: StreamBuilder<KeepState>(
+                      initialData: KeepState.unspecified,
+                      stream: store.watchState(music, fireImmediately: true),
+                      builder: (context, snapshot) {
+                        final state = snapshot.data!;
+                        return Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconActions.keptMusicAction(context, music, state),
+                            IconActions.deleteMusicAction(
+                                context, music, state),
+                            IconActions.exportActionMusic(
+                                context, music, state),
+                          ],
+                        );
+                      }),
+                );
+              }
+            },
+          );
+        }),
+      );
 }
